468d7837e904e5ffbb76f2f3a72ec1c10bc44742
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-22 22:53:37 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -380,6 +380,38 @@ TEST_CASE("selfmul matrix float") {
   REQUIRE(stroka3 == ostream.str());
 }
 
+TEST_CASE("selfmul matrix int") {
+  std::string stroka1{
+      "3, 3\n"
+      "1 1 1\n"
+      "1 1 1\n"
+      "1 1 1"};
+  std::string stroka2{
+      "3, 3\n"
+      "2 2 2\n"
+      "2 2 2\n"
+      "2 2 2"};
+  std::string stroka3{
+      "3, 3\n"
+      "6 6 6\n"
+      "6 6 6\n"
+      "6 6 6"};
+
+  matrix_t<int> matrix1, matrix2;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+
+  matrix1 *= matrix2;
+
+  std::ostringstream ostream;
+  matrix1.write(ostream);
+
+  REQUIRE(stroka3 == ostream.str());
+}
+
 TEST_CASE("Check add matrix") {
   std::string stroka1{
       "3,3\n"
