80e3a061152592b0fb044ae0b4015d4f9f1685d2
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-03-22 21:05:37 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -60,24 +60,7 @@ TEST_CASE("add matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
-TEST_CASE("Check add matrix") {
-  std::string stroka1{
-      "3,3\n"
-      "1 1 1\n"
-      "1 1 1\n"
-      "1 1 1"};
-  std::string stroka2{
-      "3, 2\n"
-      "2 2 \n"
-      "2 2 \n"
-      "2 2 "};
-  matrix_t<float> matrix1, matrix2, result;
-  std::istringstream istream1{stroka1};
-  std::istringstream istream2{stroka2};
-  matrix1.read(istream1);
-  matrix2.read(istream2);
-  REQUIRE_THROWS_AS(matrix1 + matrix2, std::invalid_argument);
-}
+
 
 TEST_CASE("sub matrix") {
   std::string stroka1{
@@ -111,24 +94,7 @@ TEST_CASE("sub matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
-TEST_CASE("Check sub matrix") {
-  std::string stroka1{
-      "3,3\n"
-      "1 1 1\n"
-      "1 1 1\n"
-      "1 1 1"};
-  std::string stroka2{
-      "3, 2\n"
-      "2 2 \n"
-      "2 2 \n"
-      "2 2 "};
-  matrix_t<float> matrix1, matrix2, result;
-  std::istringstream istream1{stroka1};
-  std::istringstream istream2{stroka2};
-  matrix1.read(istream1);
-  matrix2.read(istream2);
-  REQUIRE_THROWS_AS(matrix2 - matrix1, std::invalid_argument);
-}
+
 
 TEST_CASE("mul matrix") {
   std::string stroka1{
@@ -162,25 +128,7 @@ TEST_CASE("mul matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
-TEST_CASE("Check mul matrix") {
-  std::string stroka1{
-      "4,4\n"
-      "1 1 1 1\n"
-      "1 1 1 1\n"
-      "1 1 1 1\n"
-      "1 1 1 1"};
-  std::string stroka2{
-      "3, 2\n"
-      "2 2 \n"
-      "2 2 \n"
-      "2 2 "};
-  matrix_t<float> matrix1, matrix2, result;
-  std::istringstream istream1{stroka1};
-  std::istringstream istream2{stroka2};
-  matrix1.read(istream1);
-  matrix2.read(istream2);
-  REQUIRE_THROWS_AS(matrix1 * matrix2, std::invalid_argument);
-}
+
 
 TEST_CASE("selfsub matrix") {
   std::string stroka1{
@@ -214,26 +162,7 @@ TEST_CASE("selfsub matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
-TEST_CASE("Check selfsub matrix") {
-  std::string stroka1{
-      "3, 2\n"
-      "1 1 \n"
-      "1 1 \n"
-      "1 1 "};
-  std::string stroka2{
-      "3, 3\n"
-      "2 2 2\n"
-      "2 2 2\n"
-      "2 2 2"};
 
-  matrix_t<float> matrix1, matrix2;
-  std::istringstream istream1{stroka1};
-  std::istringstream istream2{stroka2};
-
-  matrix1.read(istream1);
-  matrix2.read(istream2);
-  REQUIRE_THROWS_AS(matrix1 -= matrix2, std::invalid_argument);
-}
 
 TEST_CASE("selfadd matrix") {
   std::string stroka1{
@@ -267,26 +196,6 @@ TEST_CASE("selfadd matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
-TEST_CASE("Check selfadd matrix") {
-  std::string stroka1{
-      "3, 2\n"
-      "1 1 \n"
-      "1 1 \n"
-      "1 1 "};
-  std::string stroka2{
-      "3, 3\n"
-      "2 2 2\n"
-      "2 2 2\n"
-      "2 2 2"};
-
-  matrix_t<float> matrix1, matrix2;
-  std::istringstream istream1{stroka1};
-  std::istringstream istream2{stroka2};
-
-  matrix1.read(istream1);
-  matrix2.read(istream2);
-  REQUIRE_THROWS_AS(matrix1 += matrix2, std::invalid_argument);
-}
 
 TEST_CASE("selfmul matrix") {
   std::string stroka1{
@@ -320,6 +229,103 @@ TEST_CASE("selfmul matrix") {
   REQUIRE(stroka3 == ostream.str());
 }
 
+
+TEST_CASE("Check add matrix") {
+  std::string stroka1{
+      "3,3\n"
+      "1 1 1\n"
+      "1 1 1\n"
+      "1 1 1"};
+  std::string stroka2{
+      "3, 2\n"
+      "2 2 \n"
+      "2 2 \n"
+      "2 2 "};
+  matrix_t<float> matrix1, matrix2, result;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+  REQUIRE_THROWS_AS(matrix1 + matrix2, std::invalid_argument);
+}
+TEST_CASE("Check sub matrix") {
+  std::string stroka1{
+      "3,3\n"
+      "1 1 1\n"
+      "1 1 1\n"
+      "1 1 1"};
+  std::string stroka2{
+      "3, 2\n"
+      "2 2 \n"
+      "2 2 \n"
+      "2 2 "};
+  matrix_t<float> matrix1, matrix2, result;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+  REQUIRE_THROWS_AS(matrix2 - matrix1, std::invalid_argument);
+}
+TEST_CASE("Check mul matrix") {
+  std::string stroka1{
+      "4,4\n"
+      "1 1 1 1\n"
+      "1 1 1 1\n"
+      "1 1 1 1\n"
+      "1 1 1 1"};
+  std::string stroka2{
+      "3, 2\n"
+      "2 2 \n"
+      "2 2 \n"
+      "2 2 "};
+  matrix_t<float> matrix1, matrix2, result;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+  REQUIRE_THROWS_AS(matrix1 * matrix2, std::invalid_argument);
+}
+TEST_CASE("Check selfsub matrix") {
+  std::string stroka1{
+      "3, 2\n"
+      "1 1 \n"
+      "1 1 \n"
+      "1 1 "};
+  std::string stroka2{
+      "3, 3\n"
+      "2 2 2\n"
+      "2 2 2\n"
+      "2 2 2"};
+
+  matrix_t<float> matrix1, matrix2;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+  REQUIRE_THROWS_AS(matrix1 -= matrix2, std::invalid_argument);
+}
+TEST_CASE("Check selfadd matrix") {
+  std::string stroka1{
+      "3, 2\n"
+      "1 1 \n"
+      "1 1 \n"
+      "1 1 "};
+  std::string stroka2{
+      "3, 3\n"
+      "2 2 2\n"
+      "2 2 2\n"
+      "2 2 2"};
+
+  matrix_t<float> matrix1, matrix2;
+  std::istringstream istream1{stroka1};
+  std::istringstream istream2{stroka2};
+
+  matrix1.read(istream1);
+  matrix2.read(istream2);
+  REQUIRE_THROWS_AS(matrix1 += matrix2, std::invalid_argument);
+}
+
 TEST_CASE("Check selfmul matrix") {
   std::string stroka1{
       "4,4\n"
@@ -339,3 +345,4 @@ TEST_CASE("Check selfmul matrix") {
   matrix2.read(istream2);
   REQUIRE_THROWS_AS(matrix1 *= matrix2, std::invalid_argument);
 }
+
