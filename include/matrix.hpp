de7835580edb4dc7386daf8e725cbc7157bf93b2
TheDonten <32191011+TheDonten@users.noreply.github.com>
2018-05-20 19:57:26 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -1,289 +1,422 @@
 #include <iostream>
+#include <cassert>
+template <typename T>
+class RBT {
+ private:
+  struct node_t {
+    node_t* parent;
+    node_t* left;
+    node_t* right;
+    T value;
+    bool color;  // черный это false, true - красный.
+  };
 
+ private:
+  node_t* root_;
 
-template <typename T> class matrix_t {
-private:
-	T ** elements_;
-	std::size_t rows_;
-	std::size_t collumns_;
-public:
-	matrix_t();
-	matrix_t( matrix_t const & other );
-	matrix_t & operator =( matrix_t const & other );
-	~matrix_t();
+ public:
+  RBT() { root_ = nullptr; }
+  node_t* root() const { return root_; }
+  node_t* grandparent(node_t* node) {
+    if ((node != nullptr) && (node->parent != nullptr))
+      return node->parent->parent;
+    else
+      return nullptr;
+  }
+  node_t* uncle(node_t* node) {
+    node_t* g = grandparent(node);
+    if (g == nullptr)
+      return nullptr;
+    if (node->parent == g->left)
+      return g->right;
+    else
+      return g->left;
+  }
+  void insert(T value) {
+    node_t* node = new node_t;
+    node->value = value;
+    node->right = nullptr;
+    node->left = nullptr;
+    if (root_ == nullptr) {
+      root_ = node;
+      root_->parent = nullptr;
+      root_->color = false;
+      return;
+    }
+    node_t* vetka = root_;
+    while (vetka != nullptr) {
+      if (vetka->value < value) {
+        if (vetka->right != nullptr) {
+          vetka = vetka->right;
+        } else {
+          vetka->right = node;
+          vetka->right->parent = vetka;
+          vetka->right->color = true;
+          insert_case1(vetka->right);
+          return;
+        }
+      } else if (vetka->value > value) {
+        if (vetka->left != nullptr) {
+          vetka = vetka->left;
+        } else {
+          vetka->left = node;
+          vetka->left->parent = vetka;
+          vetka->left->color = true;
+          insert_case1(vetka->left);
+          return;
+        }
+      } else
+        return;
+    }
+  }
+  void insert_case1(node_t* node) {
+    if (node->parent == nullptr) {
+      node->color = false;
+    } else
+      insert_case2(node);
+  }
+  void insert_case2(node_t* node) {
+    if (node->parent->color == false)
+      return;
+    else
+      insert_case3(node);
+  }
+  void insert_case3(node_t* node) {
+    node_t* u = uncle(node);
+    node_t* g;
+    if ((u != nullptr) && (u->color == true)) {
+      node->parent->color = false;
+      u->color = false;
+      g = grandparent(node);
+      g->color = true;
+      insert_case1(g);
+    } else {
+      insert_case4(node);
+    }
+  }
+  void insert_case4(node_t* node) {
+    node_t* g = grandparent(node);
+    if ((node == node->parent->right) && (node->parent == g->left)) {
+      rotate_left(node->parent);
+      node = node->left;
+    } else if ((node == node->parent->left) && (node->parent == g->right)) {
+      rotate_right(node->parent);
+      node = node->right;
+    }
+    insert_case5(node);
+  }
+  void insert_case5(node_t* node) {
+    node_t* g = grandparent(node);
+    node->parent->color = false;
+    g->color = true;
+    if ((node == node->parent->left) && (node->parent == g->left)) {
+      rotate_right(g);
+    } else if ((node == node->parent->right) && (node->parent == g->right)) {
+      rotate_left(g);
+    }
+  }
+  void rotate_left(node_t* node) {
+    node_t* vetka = node->right;
+    if (root_ == node) {
+      root_ = node->right;
+    }
+    vetka->parent = node->parent;
+    if (node->parent != nullptr) {
+      if (node->parent->left == node)
+        node->parent->left = vetka;
+      else
+        node->parent->right = vetka;
+    }
+    node->right = vetka->left;
+    if (vetka->left != nullptr)
+      vetka->left->parent = node;
+    node->parent = vetka;
+    vetka->left = node;
+  }
+  void rotate_right(node_t* node) {
+    node_t* vetka = node->left;
+    if (root_ == node) {
+      root_ = node->left;
+    }
+    vetka->parent = node->parent;
+    if (node->parent != nullptr) {
+      if (node->parent->left == node)
+        node->parent->left = vetka;
+      else
+        node->parent->right = vetka;
+    }
+    node->left = vetka->right;
+    if (vetka->right != nullptr)
+      vetka->right->parent = node;
 
-	std::size_t rows() const;
-	std::size_t collumns() const;
+    node->parent = vetka;
+    vetka->right = node;
+  }
+  node_t* sibling(node_t* node) {
+    if (node == node->parent->left)
+      return node->parent->right;
+    else
+      return node->parent->left;
+  }
+  void replace_node(node_t* node, node_t* child) {
+    node_t* g = grandparent(child);
+    node_t* child_childs;
+    if (child->left)
+      child_childs = child->left;
+    else
+      child_childs = child->right;
+    child->parent = g;
+    if (g) {
+      if (g->left == node)
+        g->left = child;
+      else
+        g->right = child;
+    }
+    if (node->right == child) {
+      if (child_childs) {
+        node->right = child_childs;
+        child_childs->parent = node;
+      }
+      child->right = node;
+      if (node->left) {
+        child->parent = node->left;
+        child->left = node->left;
+      }
+    } else {
+      if (child_childs) {
+        node->left = child_childs;
+        child_childs->parent = node;
+      }
+      child->left = node;
+      node->parent = child;
+      if (node->right) {
+        child->parent = node->right;
+        child->right = node->right;
+      }
+    }
+  }
+  void free(node_t*& node) {
+    node_t* p = node->parent;
+    node->parent = nullptr;
+    if (p->left == node) {
+      if (node->left)
+        p->left = node->left;
+      else
+        p->left = nullptr;
+    } else {
+      if (node->right)
+        p->right = node->right;
+      else
+        p->right = nullptr;
+    }
+    delete node;
+  }
+  void delete_case1(node_t* node) {
+    if (node->parent != nullptr)
+      delete_case2(node);
+  }
+  void delete_case2(node_t* node) {
+    node_t* s = sibling(node);
+    if (!(s))
+      return;
+    if (s->color == true) {
+      node->parent->color = true;
+      s->color = false;
+      if (node == node->parent->left)
+        rotate_left(node->parent);
+      else
+        rotate_right(node->parent);
+    }
+    delete_case3(node);
+  }
+  void delete_case3(node_t* node) {
+    node_t* s = sibling(node);
+    if (s->left == nullptr)
+      return;
+    else if (s->right == nullptr)
+      return;
+    if ((node->parent->color == false) && (s->color == false) &&
+        (s->left->color == false) && (s->right->color == false)) {
+      s->color = true;
+      delete_case1(node->parent);
+    } else
+      delete_case4(node);
+  }
+  void delete_case4(node_t* node) {
+    node_t* s = sibling(node);
+    if ((node->parent->color == true) && (s->color == false) &&
+        (s->left->color == false) && (s->right->color == false)) {
+      s->color = true;
+      node->parent->color = false;
+    } else
+      delete_case5(node);
+  }
+  void delete_case5(node_t* node) {
+    node_t* s = sibling(node);
+    if (s->color == false) {
+      if ((node == node->parent->left) && (s->right->color == false) &&
+          (s->left->color == true)) {
+        s->color = true;
+        s->left->color = false;
+        rotate_right(s);
+      } else if ((node == node->parent->right) && (s->left->color == false) &&
+                 (s->right->color == true)) {
+        s->color = true;
+        s->right->color = false;
+        rotate_left(s);
+      }
+    }
+    delete_case6(node);
+  }
+  void delete_case6(node_t* node) {
+    node_t* s = sibling(node);
 
-	matrix_t operator +( matrix_t const & other ) const;
-	matrix_t operator -( matrix_t const & other ) const;
-	matrix_t operator *( matrix_t const & other ) const;
+    s->color = node->parent->color;
+    node->parent->color = false;
+    if (node == node->parent->left) {
+      s->right->color = false;
+      rotate_left(node->parent);
+    } else
+      s->left->color = false;
+    rotate_right(node->parent);
+  }
+  void delete_one_child(node_t* node, node_t* child) {
+    if (child) {
+      replace_node(node, child);
+      if (node->color == false) {
+        if (child->color == true)
+          child->color = false;
+        else
+          delete_case1(child);
+      }
+    }
+    free(node);
+  }
+  bool remove(int key) {
+    if (!(root_)) {
+      throw std::invalid_argument("root_ = nullptr");
+    }
+    node_t* vetka = root_;
+    while (vetka) {
+      if (vetka->value == key)
+        break;
+      else {
+        if (key < vetka->value)
+          vetka = vetka->left;
+        else
+          vetka = vetka->right;
+      }
+      if (!(vetka))
+        return false;
+    }
+    if (vetka) {
+      if (vetka->left == nullptr && vetka->right != nullptr) {
+        delete_one_child(vetka, vetka->right);
+      } else if (vetka->right == nullptr && vetka->left != nullptr) {
+        delete_one_child(vetka, vetka->left);
+      } else if (vetka->left == nullptr && vetka->right == nullptr) {
+        delete_case1(vetka);
+        free(vetka);
+      } else if (vetka->right && vetka->left) {
+        if (vetka->color == true) {
+          node_t* child = choose_left_child(vetka);
+          if (child->color == true) {
+            swap_child_parent(vetka, child);
+            if (!(vetka->left) && !(vetka->right))
+              free(vetka);
+            else if (vetka->right) {
+              delete_one_child(vetka, vetka->right);
+            }
+          } else if (child->color == false) {
+            swap_child_parent(vetka, child);
+            if (!(vetka->left) && !(vetka->right))
+              free(vetka);
+            else if (vetka->right) {
+              delete_one_child(vetka, vetka->right);
+            }
+          }
+        } else if (vetka->color == false) {
+          node_t* child = choose_left_child(vetka);
+          if (child->color == true) {
+            swap_child_parent(vetka, child);
+            child->color = false;
+            if (!(vetka->left) && !(vetka->right))
+              free(vetka);
+            else if (vetka->right)
+              delete_one_child(vetka, vetka->right);
+          } else if (child->color == false) {
+            swap_child_parent(vetka, child);
+            if (!(vetka->left) && !(vetka->right)) {
+              delete_case1(vetka);
+              free(vetka);
+            } else if (vetka->right)
+              delete_one_child(vetka, vetka->right);
+          }
+        }
+      }
+    } else
+      return false;
+  }
+  node_t* choose_left_child(node_t* node) {
+    node_t* vetka = node->right;
+    while (vetka->left) {
+      vetka = vetka->left;
+    }
+    return vetka;
+  }
+  void swap_child_parent(node_t* node, node_t* child) {
+    node_t* node_parent = node->parent;
+    node_t* parent_child = child->parent;
+    node_t* right_child = node->right;
+    child->parent = node_parent;
+    if (node_parent) {
+      if (node_parent->right == node) {
+        node_parent->right = child;
+      } else {
+        node_parent->left = child;
+      }
+    } else
+      root_ = child;
+    node->right = child->right;
+    if (node->left) {
+      node->left->parent = child;
+      child->left = node->left;
+      node->left = nullptr;
+    }
 
-	matrix_t & operator -=( matrix_t const & other );
-	matrix_t & operator +=( matrix_t const & other );
-	matrix_t & operator *=( matrix_t const & other );
+    if (node != parent_child) {
+      parent_child->left = node;
+      node->parent = parent_child;
+      child->right = right_child;
+      right_child->parent = child;
+    } else {
+      node->left = child->right;
+      child->right = node;
+      node->parent = child;
+    }
+  }
+  void print(std::ostream& stream, int level, node_t* node) const {
+    if (node == nullptr)
+      return;
 
-	std::istream & read( std::istream & stream );
-	std::ostream & write( std::ostream  & stream ) const;
+    print(stream, level + 1, node->right);
+
+    for (unsigned int i = 0; i < level; i++) {
+      stream << "---";
+    }
+    if (node->color == false)
+      stream << node->value << "B" << std::endl;
+    if (node->color == true)
+      stream << node->value << "R" << std::endl;
+    print(stream, level + 1, node->left);
+  }
+  void destroy(node_t* node) {
+    if (node != nullptr) {
+      destroy(node->left);
+      destroy(node->right);
+      delete node;
+    }
+  }
+  ~RBT() { destroy(root_); }
 };
 
-template <typename T>
-matrix_t<T>::matrix_t() : elements_{ nullptr }, rows_{ 0 }, collumns_{ 0 }
-{
-}
-template <typename T>
-matrix_t<T>::matrix_t(matrix_t const & other)
-{
-	rows_ = other.rows_;
-	collumns_ = other.collumns_;
-	elements_ = new T *[rows_];
-	for (std::size_t i = 0; i < rows_; ++i) {
-		elements_[i] = new T[this->collumns_];
-		for (std::size_t j = 0; j < collumns_; ++j) {
-			elements_[i][j] = other.elements_[i][j];
-		}
-	}
-}
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator =(matrix_t const & other)
-{
-	if (this != &other) {
-			for (std::size_t i = 0; i < rows_; ++i) {
-				delete[] elements_[i];
-			}
-			delete[] elements_;
-
-			rows_ = other.rows_;
-			collumns_ = other.collumns_;
-			elements_ = new T *[rows_];
-			for (std::size_t i = 0; i < rows_; ++i) {
-				elements_[i] = new T[collumns_];
-			}
-			for (std::size_t i = 0; i < rows_; ++i) {
-				for (std::size_t j = 0; j < collumns_; ++j) {
-					elements_[i][j] = other.elements_[i][j];
-				}
-			}
-		}
-
-	return *this;
-}
-template <typename T>
-matrix_t<T>::~matrix_t()
-{
-	for (size_t i = 0; i < this->rows_; i++)
-	{
-		delete[] this->elements_[i];
-	}
-	delete[] this->elements_;
-	collumns_ = 0;
-	rows_ = 0;
-}
-template <typename T>
-std::size_t matrix_t<T>::rows() const
-{
-	return rows_;
-}
-template <typename T>
-std::size_t matrix_t<T>::collumns() const
-{
-	return collumns_;
-}
-template <typename T>
-matrix_t<T> matrix_t<T>::operator +(matrix_t const & other) const
-{
-	matrix_t<T> result;
-
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		
-		result.rows_ = rows_;
-		result.collumns_ = collumns_;
-                result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T[collumns_];
-			for (std::size_t j = 0; j<collumns_; j++) {
-				result.elements_[i][j] = elements_[i][j] + other.elements_[i][j];
-			}
-		}
-	}
-	else {
-		throw std::invalid_argument("rows and collumns != other.rows and other.collumns");
-	}
-
-	return result;
-}
-template <typename T>
-matrix_t<T> matrix_t<T>::operator -(matrix_t const & other) const
-{
-	matrix_t<T> result;
-
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		
-		result.rows_ = rows_;
-		result.collumns_ = collumns_;
-                result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i<rows_; i++) {
-			result.elements_[i] = new T[collumns_];
-			for (std::size_t j = 0; j<collumns_; j++) {
-				result.elements_[i][j] = elements_[i][j] - other.elements_[i][j];
-			}
-		}
-	}
-	else {
-		throw std::invalid_argument("rows and collumns != other.rows and other.collumns");
-	}
-
-	return result;
-}
-template <typename T>
-matrix_t<T> matrix_t<T>::operator *(matrix_t const & other) const
-{
-	matrix_t<T> result;
-
-	if (collumns_ == other.rows_) {
-		
-		result.rows_ = rows_;
-		result.collumns_ = other.collumns_;
-                result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i < rows_; ++i) {
-			 result.elements_ [i] = new T[other.collumns_];
-			
-			for (std::size_t j = 0; j < other.collumns_; ++j) {
-				size_t result_ = 0;
-				for (std::size_t k = 0; k < other.rows_; ++k) {
-					result_ += elements_[i][k] * other.elements_[k][j];
-				}
-				result.elements_[i][j] = result_;
-			}
-		}
-	}
-	else {
-		throw std::invalid_argument("collumns != other.rows");
-	}
-
-	return result;
-}
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator -=(matrix_t const & other)
-{
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
-				elements_[i][j] -= other.elements_[i][j];
-			}
-		}
-	}
-	else {
-		throw std::invalid_argument("rows and collumns != other.rows and other.collumns");
-	}
-
-	return *this;
-}
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator +=(matrix_t const & other)
-{
-	if (rows_ == other.rows_ && collumns_ == other.collumns_) {
-		for (std::size_t i = 0; i<rows_; i++) {
-			for (std::size_t j = 0; j<collumns_; j++) {
-				elements_[i][j] += other.elements_[i][j];
-			}
-		}
-	}
-	else {
-		throw std::invalid_argument("rows and collumns != other.rows and other.collumns");
-	}
-
-	return *this;
-}
-template <typename T>
-matrix_t<T> & matrix_t<T>::operator *=(matrix_t const & other)
-{
-	matrix_t result;
-
-	if (collumns_ == other.rows_) {
-		
-		result.rows_ = rows_;
-		result.collumns_ = other.collumns_;
-                result.elements_ = new T *[rows_];
-		for (std::size_t i = 0; i < rows_; ++i) {
-			result.elements_[i] = new T[other.collumns_];
-			for (std::size_t j = 0; j < other.collumns_; ++j) {
-                          size_t result_ = 0;
-				for (std::size_t k = 0; k < other.rows_; ++k) {
-					result_ += elements_[i][k] * other.elements_[k][j];
-				}
-				result.elements_[i][j] = result_;
-			}
-		}
-		*this = result;
-	}
-	else {
-		throw std::invalid_argument("collumns != other.rows");
-	}
-
-	return *this;
-}
-template <typename T>
-std::istream & matrix_t<T>::read(std::istream & stream)
-{
-	std::size_t rows;
-	std::size_t collumns;
-	char symbol;
-
-	bool success = true;
-	if (stream >> rows && stream >> symbol && symbol == ',' && stream >> collumns) {
-		T ** elements = new T *[rows];
-		for (std::size_t i = 0; success && i < rows; ++i) {
-			elements[i] = new T[collumns];
-			for (std::size_t j = 0; j < collumns; ++j) {
-				if (!(stream >> elements[i][j])) {
-					success = false;
-					break;
-				}
-			}
-		}
-
-		if (success) {
-			for (std::size_t i = 0; i < rows_; ++i) {
-				delete[] elements_[i];
-			}
-			delete[] elements_;
-
-			rows_ = rows;
-			collumns_ = collumns;
-			elements_ = elements;
-		}
-		else {
-			for (std::size_t i = 0; i < rows; ++i) {
-				delete[] elements[i];
-			}
-			delete[] elements;
-		}
-	}
-	else {
-		success = false;
-	}
-
-	if (!success) {
-		stream.setstate(std::ios_base::failbit);
-	}
-
-	return stream;
-}
-template <typename T>
-std::ostream & matrix_t<T>::write(std::ostream & stream) const
-{
-	stream << rows_ << ", " << collumns_;
-	for (std::size_t i = 0; i < rows_; ++i) {
-		stream << '\n';
-		for (std::size_t j = 0; j < collumns_; ++j) {
-			stream << elements_[i][j];
-			if (j != collumns_ - 1) {
-				stream << ' ';
-			}
-		}
-	}
-	if (collumns_ != rows_) stream << '\n';
-
-	return stream;
-}
